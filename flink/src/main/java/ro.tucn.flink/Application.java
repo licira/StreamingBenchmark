05ee416424a586ffb746e2c51471eaf389f34c2a
Liviu <li.cira@yaho.com>
2017-04-16 22:56:43 +0300

    Added Flink module.

---
+++ flink/src/main/java/ro.tucn.flink/Application.java
@@ -0,0 +1,11 @@
+package ro.tucn.flink;
+
+/**
+ * Created by Liviu on 4/16/2017.
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hi from Flink");
+    }
+}
