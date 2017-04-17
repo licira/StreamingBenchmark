4ab5b0ac03c9feef5c0e3324443fe6388d91d74d
Liviu <li.cira@yaho.com>
2017-04-17 15:38:56 +0300

    Added part of Flink Implementation.

---
+++ flink/src/main/java/ro.tucn.flink/function/MapFunctionWithInitList.java
@@ -0,0 +1,25 @@
+package ro.tucn.flink.function;
+
+import org.apache.flink.api.common.functions.MapFunction;
+import ro.tucn.frame.functions.MapWithInitListFunction;
+
+import java.util.List;
+
+/**
+ * Created by Liviu on 4/17/2017.
+ */
+public class MapFunctionWithInitList<T, R> implements MapFunction<T, R> {
+
+    private List<T> initList;
+    private MapWithInitListFunction<T, R> function;
+
+    public MapFunctionWithInitList(MapWithInitListFunction<T, R> fun, List<T> initList) {
+        this.function = fun;
+        this.initList = initList;
+    }
+
+    @Override
+    public R map(T t) throws Exception {
+        return null;
+    }
+}
