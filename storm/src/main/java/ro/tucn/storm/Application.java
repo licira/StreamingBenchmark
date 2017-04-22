0db00aa99c6ffa55c1393fc25f40a6e9d906f52b
Liviu <li.cira@yaho.com>
2017-04-22 17:16:42 +0300

    Added Storm implementation.

--- storm/src/main/java/ro/tucn/storm/Application.java
+++ storm/src/main/java/ro/tucn/storm/Application.java
@@ -16,23 +16,25 @@ import java.io.IOException;
 public class Application {
 
     public static void main(String[] args) throws IOException, WorkloadException {
-        OperatorCreator operatorCreator;
-        Workload workload = null;
-        if (args[0].equalsIgnoreCase(Topics.ADV)) {
-            operatorCreator = new StormOperatorCreator(Topics.ADV);
-            workload = new AdvClick(operatorCreator);
-        } else if (args[0].equalsIgnoreCase(Topics.K_MEANS)) {
-            operatorCreator = new StormOperatorCreator(Topics.K_MEANS);
-            workload = new KMeans(operatorCreator);
-        } else if (args[0].equalsIgnoreCase(Topics.UNIFORM_WORDS)) {
-            operatorCreator = new StormOperatorCreator(Topics.UNIFORM_WORDS);
-            workload = new AdvClick(operatorCreator);
-        } else if (args[0].equalsIgnoreCase(Topics.SKEWED_WORDS)) {
-            operatorCreator = new StormOperatorCreator(Topics.SKEWED_WORDS);
-            workload = new AdvClick(operatorCreator);
-        } else {
-            return;
+        if (args.length > 0) {
+            OperatorCreator operatorCreator;
+            Workload workload = null;
+            if (args[0].equalsIgnoreCase(Topics.ADV)) {
+                operatorCreator = new StormOperatorCreator(Topics.ADV);
+                workload = new AdvClick(operatorCreator);
+            } else if (args[0].equalsIgnoreCase(Topics.K_MEANS)) {
+                operatorCreator = new StormOperatorCreator(Topics.K_MEANS);
+                workload = new KMeans(operatorCreator);
+            } else if (args[0].equalsIgnoreCase(Topics.UNIFORM_WORDS)) {
+                operatorCreator = new StormOperatorCreator(Topics.UNIFORM_WORDS);
+                workload = new AdvClick(operatorCreator);
+            } else if (args[0].equalsIgnoreCase(Topics.SKEWED_WORDS)) {
+                operatorCreator = new StormOperatorCreator(Topics.SKEWED_WORDS);
+                workload = new AdvClick(operatorCreator);
+            } else {
+                return;
+            }
+            workload.Start();
         }
-        workload.Start();
     }
 }
