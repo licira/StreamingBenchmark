fd6ebe25e171861bb75233460c1e1f2e1dd6bb6f
Liviu <li.cira@yaho.com>
2017-05-06 23:12:25 +0300

    Small changes.

--- utils/src/main/java/ro.tucn/workload/Workload.java
+++ utils/src/main/java/ro.tucn/workload/Workload.java
@@ -3,8 +3,8 @@ package ro.tucn.workload;
 import org.apache.log4j.Logger;
 import ro.tucn.exceptions.WorkloadException;
 import ro.tucn.kMeans.Point;
-import ro.tucn.operator.OperatorCreator;
 import ro.tucn.operator.Operator;
+import ro.tucn.operator.OperatorCreator;
 import ro.tucn.util.Configuration;
 import ro.tucn.util.WithTime;
 
@@ -24,28 +24,13 @@ public abstract class Workload implements Serializable {
     protected int parallelism;
     private OperatorCreator operatorCreator;
 
-    public abstract void process() throws WorkloadException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException;
-
     public Workload(OperatorCreator operatorCreator) throws WorkloadException {
         this.operatorCreator = operatorCreator;
-        Configuration.loadConfiguration();
-        parallelism = Configuration.clusterHosts * Configuration.hostCores;
-        // load specific configure for each workload
-        properties = new Properties();
-        String configFile = this.getClass().getSimpleName() + ".properties";
-        try {
-            logger.info("Loading Workload properties: " + configFile);
-            properties.load(this.getClass().getClassLoader().getResourceAsStream(configFile));
-            logger.info("Properties loaded for: " + configFile);
-            //int hosts = Integer.parseInt(properties.getProperty("hosts"));
-            //int cores = Integer.parseInt(properties.getProperty("cores"));
-        } catch (IOException e) {
-            throw new WorkloadException("Read configure file " + configFile + " failed");
-        } catch (Exception e) {
-            e.printStackTrace();
-            logger.error("Read configure file: " + configFile + " failed");
-        }
+        initializeParallelism();
+        initializeProperties();
     }
+
+    public abstract void process() throws WorkloadException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException;
 
     public void Start() {
         logger.info("Start workload: " + this.getClass().getSimpleName());
@@ -57,6 +42,25 @@ public abstract class Workload implements Serializable {
                     + ": " + e.getClass() + " " + e.getMessage());
         }
         logger.info("The end of workload: " + this.getClass().getSimpleName());
+    }
+
+    private void initializeProperties() throws WorkloadException {
+        properties = new Properties();
+        String configFile = this.getClass().getSimpleName() + ".properties";
+        try {
+            properties.load(this.getClass().getClassLoader().getResourceAsStream(configFile));
+            //int hosts = Integer.parseInt(properties.getProperty("hosts"));
+            //int cores = Integer.parseInt(properties.getProperty("cores"));
+        } catch (IOException e) {
+            throw new WorkloadException("Read configure file " + configFile + " failed");
+        } catch (Exception e) {
+            logger.error("Read configure file: " + configFile + " failed");
+        }
+    }
+
+    private void initializeParallelism() throws WorkloadException {
+        Configuration.loadConfiguration();
+        parallelism = Configuration.clusterHosts * Configuration.hostCores;
     }
 
     protected Operator<String> getStringStreamOperator(String componentId, String topicPropertyName) {
