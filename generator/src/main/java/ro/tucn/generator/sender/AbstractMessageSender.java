92a4faf1545ec85b5639d74036eb97aff4de41da
Liviu <li.cira@yaho.com>
2017-06-16 22:50:22 +0300

    Decommented line.

--- generator/src/main/java/ro/tucn/generator/sender/AbstractMessageSender.java
+++ generator/src/main/java/ro/tucn/generator/sender/AbstractMessageSender.java
@@ -43,7 +43,7 @@ public abstract class AbstractMessageSender implements Serializable {
     public void send(String topic, Object key, Object value) {
         long timestamp = TimeHelper.getNanoTime();
         newRecord = new ProducerRecord(topic, null, timestamp, key, value);
-        //producer.send(newRecord);
+        producer.send(newRecord);
         logger.info("Topic: " + topic + "\tMessage: " + newRecord.value());
     }
 
