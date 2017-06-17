6f97818a49c497b16de6094d88b1ba23687ed216
Liviu <li.cira@yaho.com>
2017-06-17 15:42:47 +0300

    Small change in toString method.

--- utils/src/main/java/ro.tucn/kMeans/Point.java
+++ utils/src/main/java/ro.tucn/kMeans/Point.java
@@ -9,28 +9,28 @@ public class Point implements Serializable {
 
     public int id; // Centroid id: 0, 1, 2, ...
     public double[] coordinates;
-    private long time;
+    private Long timestamp;
 
     public Point() { }
 
     public Point(int id, double[] coordinates) {
         this.id = id;
         this.coordinates = coordinates;
-        this.time = System.nanoTime();
+        this.timestamp = System.nanoTime();
     }
 
-    public Point(int id, double[] l, long time) {
+    public Point(int id, double[] l, long timestamp) {
         this(id, l);
-        this.time = time;
+        this.timestamp = timestamp;
     }
 
     public Point(double[] l) {
         this(-1, l);
     }
 
-    public Point(double[] l, long time) {
+    public Point(double[] l, long timestamp) {
         this(-1, l);
-        this.time = time;
+        this.timestamp = timestamp;
     }
 
     public int dimension() {
@@ -42,11 +42,11 @@ public class Point implements Serializable {
     }
 
     public long getTime() {
-        return this.time;
+        return this.timestamp;
     }
 
-    public void setTime(long time) {
-        this.time = time;
+    public void setTime(long timestamp) {
+        this.timestamp = timestamp;
     }
 
     /*public Point add(Point other) throws Exception {
@@ -57,7 +57,7 @@ public class Point implements Serializable {
         for (int i = 0; i < this.coordinates.length; ++i) {
             coordinates[i] = this.coordinates[i] + other.coordinates[i];
         }
-        return new Point(this.id, coordinates, this.time);
+        return new Point(this.id, coordinates, this.timestamp);
     }*/
 
     /*public Point mul(long val) {
@@ -65,7 +65,7 @@ public class Point implements Serializable {
         for (int i = 0; i < this.coordinates.length; ++i) {
             coordinates[i] = this.coordinates[i] * val;
         }
-        return new Point(this.id, coordinates, this.time);
+        return new Point(this.id, coordinates, this.timestamp);
     }*/
 
     /*public Point div(long val) {
@@ -73,7 +73,7 @@ public class Point implements Serializable {
         for (int i = 0; i < this.coordinates.length; ++i) {
             coordinates[i] = this.coordinates[i] / val;
         }
-        return new Point(this.id, coordinates, this.time);
+        return new Point(this.id, coordinates, this.timestamp);
     }*/
 
     public double euclideanDistance(Point other) {
@@ -86,6 +86,44 @@ public class Point implements Serializable {
             squareSum += Math.pow(this.coordinates[i] - other.coordinates[i], 2);
         }
         return Math.sqrt(squareSum);
+    }
+
+    public Point add(Point point) {
+        double[] additionPointCoordinates = point.getCoordinates();
+        Point result = new Point();
+        double coordinates[] = new double[this.coordinates.length];
+        for (int i = 0; i < this.coordinates.length - 1; i++) {
+            coordinates[i] = this.coordinates[i] + additionPointCoordinates[i];
+        }
+        result.setCoordinates(coordinates);
+        return result;
+    }
+
+    public Point div(Long divider) {
+        Point result = new Point();
+        double coordinates[] = new double[this.coordinates.length];
+        for (int i = 0; i < this.coordinates.length - 1; ++i) {
+            coordinates[i] = this.coordinates[i] / divider;
+        }
+        result.setCoordinates(coordinates);
+        return result;
+    }
+
+    public String toString() {
+        String str = "";
+        str += "id: " + id + " ";
+        str += "coodrdinates: [";
+        if (coordinates != null) {
+            int dimensions = this.coordinates.length;
+            int i;
+            for (i = 0; i < dimensions - 1; i++) {
+                str += this.coordinates[i] + ", ";
+            }
+            str += this.coordinates[i];
+        }
+        str += "], ";
+        str += "timestamp: " + timestamp;
+        return str;
     }
 
     public void setCoordinates(double[] coordinates) {
@@ -102,37 +140,5 @@ public class Point implements Serializable {
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public String toString() {
-        String str = "";
-        for (int i = 0; i < this.coordinates.length - 1; ++i) {
-            str += this.coordinates[i] + " ";
-        }
-        str += this.coordinates[this.coordinates.length - 1];
-        if (-1 != this.id)
-            return id + ":" + str;
-        return str;
-    }
-
-    public Point add(Point point) {
-        double[] additionPointCoordinates = point.getCoordinates();
-        Point result = new Point();
-        double coordinates[] = new double[this.coordinates.length];
-        for (int i = 0; i < this.coordinates.length - 1; ++i) {
-            coordinates[i] = this.coordinates[i] + additionPointCoordinates[i];
-        }
-        result.setCoordinates(coordinates);
-        return result;
-    }
-
-    public Point div(Long divider) {
-        Point result = new Point();
-        double coordinates[] = new double[this.coordinates.length];
-        for (int i = 0; i < this.coordinates.length - 1; ++i) {
-            coordinates[i] = this.coordinates[i] / divider;
-        }
-        result.setCoordinates(coordinates);
-        return result;
     }
 }
