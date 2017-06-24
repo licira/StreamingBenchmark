2b47deeadebbf363ac8b766220ec5955c63ba177
Liviu <li.cira@yaho.com>
2017-06-24 19:33:13 +0300

    Removed unnecessary methods.

--- utils/src/main/java/ro.tucn/frame/functions/FlatMapFunction.java
+++ utils/src/main/java/ro.tucn/frame/functions/FlatMapFunction.java
@@ -5,12 +5,6 @@ import java.util.Iterator;
 /**
  * Created by Liviu on 4/8/2017.
  */
-// flink
-//public interface FlatMapFunction {
-//    public void flatMap(String value, Iterable<Tuple2<String, Integer>> out);
-//}
-
-// spark
 public interface FlatMapFunction<T, R> extends SerializableFunction {
 
     Iterator<R> flatMap(T var1) throws Exception;
