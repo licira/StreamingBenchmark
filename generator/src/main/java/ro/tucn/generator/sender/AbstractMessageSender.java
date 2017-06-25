a52fa85b068a57756ee099cc05cfc714c6642596
Liviu <li.cira@yaho.com>
2017-06-25 18:02:03 +0300

    Small change.

--- generator/src/main/java/ro/tucn/generator/sender/AbstractMessageSender.java
+++ generator/src/main/java/ro/tucn/generator/sender/AbstractMessageSender.java
@@ -14,9 +14,9 @@ import java.io.Serializable;
  */
 public abstract class AbstractMessageSender implements Serializable {
 
+    protected final Logger logger = Logger.getLogger(this.getClass().getSimpleName());
     protected static KafkaProducer<String, String> producer;
     protected static ProducerRecord<String, String> newRecord;
-    protected final Logger logger = Logger.getLogger(this.getClass().getSimpleName());
     protected String topic;
     private ProducerCreator producerCreator;
     private Gson gson;
