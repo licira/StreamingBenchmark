99cbe6506448d970f2b028fe18fde7e2b6b6a4a9
Liviu <li.cira@yaho.com>
2017-06-27 17:44:28 +0300

    Introduced ArgParser in Flink and Spark application.

--- flink/src/main/java/ro.tucn.flink/Application.java
+++ flink/src/main/java/ro.tucn.flink/Application.java
@@ -3,6 +3,7 @@ package ro.tucn.flink;
 import ro.tucn.exceptions.WorkloadException;
 import ro.tucn.flink.operator.FlinkContextCreator;
 import ro.tucn.operator.ContextCreator;
+import ro.tucn.util.ArgsParser;
 import ro.tucn.util.Topics;
 import ro.tucn.workload.*;
 import ro.tucn.workload.stream.AdvClickStream;
@@ -11,6 +12,7 @@ import ro.tucn.workload.stream.WordCountStream;
 import ro.tucn.workload.stream.WordCountFastStream;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 /**
  * Created by Liviu on 4/16/2017.
@@ -19,18 +21,20 @@ public class Application {
 
     public static void main(String[] args) throws IOException, WorkloadException {
         if (args.length > 0) {
+            HashMap<String, String> parsedArgs = ArgsParser.parseArgs(args);
+            String topic = ArgsParser.getTopic(parsedArgs);
             ContextCreator contextCreator;
             Workload workload = null;
-            if (args[0].equalsIgnoreCase(Topics.ADV)) {
+            if (topic.equalsIgnoreCase(Topics.ADV)) {
                 contextCreator = new FlinkContextCreator(Topics.ADV);
                 workload = new AdvClickStream(contextCreator);
-            } else if (args[0].equalsIgnoreCase(Topics.K_MEANS)) {
+            } else if (topic.equalsIgnoreCase(Topics.K_MEANS)) {
                 contextCreator = new FlinkContextCreator(Topics.K_MEANS);
                 workload = new KMeansStream(contextCreator);
-            } else if (args[0].equalsIgnoreCase(Topics.UNIFORM_WORDS)) {
+            } else if (topic.equalsIgnoreCase(Topics.UNIFORM_WORDS)) {
                 contextCreator = new FlinkContextCreator(Topics.UNIFORM_WORDS);
                 workload = new WordCountStream(contextCreator);
-            } else if (args[0].equalsIgnoreCase(Topics.SKEWED_WORDS)) {
+            } else if (topic.equalsIgnoreCase(Topics.SKEWED_WORDS)) {
                 contextCreator = new FlinkContextCreator(Topics.SKEWED_WORDS);
                 workload = new WordCountFastStream(contextCreator);
             } else {
--- spark/src/main/java/ro/tucn/spark/Application.java
+++ spark/src/main/java/ro/tucn/spark/Application.java
@@ -3,6 +3,7 @@ package ro.tucn.spark;
 import ro.tucn.exceptions.WorkloadException;
 import ro.tucn.operator.ContextCreator;
 import ro.tucn.spark.operator.SparkContextCreator;
+import ro.tucn.util.ArgsParser;
 import ro.tucn.util.Topics;
 import ro.tucn.workload.*;
 import ro.tucn.workload.stream.AdvClickStream;
@@ -11,6 +12,7 @@ import ro.tucn.workload.stream.WordCountStream;
 import ro.tucn.workload.stream.WordCountFastStream;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 /**
  * Created by Liviu on 4/7/2017.
@@ -20,19 +22,21 @@ public class Application {
     public static void main(String[] args) throws IOException, WorkloadException {
         if (args.length > 0)
         {
+            HashMap<String, String> parsedArgs = ArgsParser.parseArgs(args);
+            String topic = ArgsParser.getTopic(parsedArgs);
             ContextCreator contextCreator;
             Workload workload = null;
-            if (args[0].equalsIgnoreCase(Topics.ADV))
+            if (topic.equalsIgnoreCase(Topics.ADV))
             {
                 contextCreator = new SparkContextCreator(Topics.ADV);
                 workload = new AdvClickStream(contextCreator);
-            } else if (args[0].equalsIgnoreCase(Topics.K_MEANS)) {
+            } else if (topic.equalsIgnoreCase(Topics.K_MEANS)) {
                 contextCreator = new SparkContextCreator(Topics.K_MEANS);
                 workload = new KMeansStream(contextCreator);
-            } else if (args[0].equalsIgnoreCase(Topics.UNIFORM_WORDS)) {
+            } else if (topic.equalsIgnoreCase(Topics.UNIFORM_WORDS)) {
                 contextCreator = new SparkContextCreator(Topics.UNIFORM_WORDS);
                 workload = new WordCountStream(contextCreator);
-            } else if (args[0].equalsIgnoreCase(Topics.SKEWED_WORDS)) {
+            } else if (topic.equalsIgnoreCase(Topics.SKEWED_WORDS)) {
                 contextCreator = new SparkContextCreator(Topics.SKEWED_WORDS);
                 workload = new WordCountFastStream(contextCreator);
             } else {
