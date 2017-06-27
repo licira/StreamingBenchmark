39c8ed088212b0fe50ce55ca96dec4a00e8e5c81
Liviu <li.cira@yaho.com>
2017-06-27 12:00:20 +0300

    Deleted unnecessary methods.

--- utils/src/main/java/ro.tucn/operator/BatchPairOperator.java
+++ utils/src/main/java/ro.tucn/operator/BatchPairOperator.java
@@ -1,7 +1,5 @@
 package ro.tucn.operator;
 
-import ro.tucn.exceptions.UnsupportOperatorException;
-
 /**
  * Created by Liviu on 6/25/2017.
  */
@@ -9,15 +7,5 @@ public abstract class BatchPairOperator<K, V> extends BaseOperator {
 
     public BatchPairOperator(int parallelism) {
         super(parallelism);
-    }
-
-    @Override
-    public void closeWith(BaseOperator stream, boolean broadcast) throws UnsupportOperatorException {
-
-    }
-
-    @Override
-    public void print() {
-
     }
 }
