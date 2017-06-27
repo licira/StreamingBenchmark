0dbbae65c072f8132a73e0b3e2b9862fd8004dcc
Liviu <li.cira@yaho.com>
2017-06-27 20:30:17 +0300

    Created GeneratorConsumer.

---
+++ utils/src/main/java/ro.tucn/consumer/GeneratorConsumer.java
@@ -0,0 +1,28 @@
+package ro.tucn.consumer;
+
+import ro.tucn.kMeans.Point;
+import ro.tucn.operator.BatchOperator;
+import ro.tucn.operator.BatchPairOperator;
+import ro.tucn.util.TimeHolder;
+
+import java.util.Properties;
+
+/**
+ * Created by Liviu on 6/27/2017.
+ */
+public abstract class GeneratorConsumer {
+
+    protected int parallelism;
+
+    public abstract BatchPairOperator<String, String> getBatchPairOperator(Properties properties,
+                                                                           String topicPropertyName);
+
+    public abstract BatchOperator<TimeHolder<String>> getBatchStringOperatorWithTimeHolder(Properties properties,
+                                                                                           String topicPropertyName);
+
+    public abstract BatchOperator<Point> getBatchPointOperator(Properties properties,
+                                                               String topicPropertyName);
+
+    public abstract BatchOperator<String> getBatchStringOperator(Properties properties,
+                                                                 String topicPropertyName);
+}
