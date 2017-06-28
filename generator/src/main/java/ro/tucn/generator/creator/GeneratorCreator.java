e5a27584b47f0bf153e8a6f321ad702983d897ce
Liviu <li.cira@yaho.com>
2017-06-28 12:15:51 +0300

    Small change.

--- generator/src/main/java/ro/tucn/generator/creator/GeneratorCreator.java
+++ generator/src/main/java/ro/tucn/generator/creator/GeneratorCreator.java
@@ -11,16 +11,19 @@ public class GeneratorCreator {
 
     private static final String NONEXISTING_GENERATOR_EXCEPTION_MSG ="No generator available for this.";
 
-    public static AbstractGenerator getNewGenerator(String topic, int entitiesNumber) {
+    public static AbstractGenerator getNewGenerator(String topic, String mode, int entitiesNumber) {
         AbstractGenerator generator;
+        if (mode == null) {
+            mode = DataMode.STREAMING;
+        }
         if (topic.equalsIgnoreCase(String.valueOf(ApplicationTopics.ADV))) {
-            generator = new AdvClickGenerator(DataMode.STREAMING, entitiesNumber);
+            generator = new AdvClickGenerator(mode, entitiesNumber);
         } else if (topic.equalsIgnoreCase(String.valueOf(ApplicationTopics.K_MEANS))) {
-            generator = new KMeansGenerator(DataMode.STREAMING, entitiesNumber);
+            generator = new KMeansGenerator(mode, entitiesNumber);
         } else if (topic.equalsIgnoreCase(String.valueOf(ApplicationTopics.SKEWED_WORDS))) {
-            generator = new SkewedWordsGenerator(DataMode.STREAMING, entitiesNumber);
+            generator = new SkewedWordsGenerator(mode, entitiesNumber);
         } else if (topic.equalsIgnoreCase(String.valueOf(ApplicationTopics.UNIFORM_WORDS))) {
-            generator = new UniformWordsGenerator(DataMode.STREAMING, entitiesNumber);
+            generator = new UniformWordsGenerator(mode, entitiesNumber);
         } else {
             throw new RuntimeException(NONEXISTING_GENERATOR_EXCEPTION_MSG);
         }
