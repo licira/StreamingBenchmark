53674741d674f01f9c32bbfa4f24a6f6479dcb98
Liviu <li.cira@yaho.com>
2017-06-28 16:40:47 +0300

    Small changes.

--- generator/src/main/java/ro/tucn/generator/GeneratorProducer.java
+++ generator/src/main/java/ro/tucn/generator/GeneratorProducer.java
@@ -10,17 +10,19 @@ import java.util.Map;
  */
 public class GeneratorProducer<K, V> {
 
-    Map<String, List<Map<K, V>>> buffer = new HashMap<>();
+    private Map<String, List<Map<K, V>>> buffer = new HashMap<>();
 
     public void send(GeneratorRecord<K, V> record) {
         String topic = record.getTopic();
         List<Map<K, V>> kvList = buffer.get(topic);
+        buffer.remove(topic);
         if (kvList == null) {
             kvList = new ArrayList<>();
         }
         Map<K, V> kvMap = new HashMap<>();
         kvMap.put(record.getKey(), record.getValue());
         kvList.add(kvMap);
+        buffer.put(topic, kvList);
     }
 
     public List<Map<K, V>> read(String topic) {
