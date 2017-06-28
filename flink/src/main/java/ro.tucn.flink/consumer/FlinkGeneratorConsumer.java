239a6134f68a8c124bb66f2bd9bd26df97f55ad5
Liviu <li.cira@yaho.com>
2017-06-28 17:53:03 +0300

    Implementation.

--- flink/src/main/java/ro.tucn.flink/consumer/FlinkGeneratorConsumer.java
+++ flink/src/main/java/ro.tucn.flink/consumer/FlinkGeneratorConsumer.java
@@ -1,12 +1,25 @@
 package ro.tucn.flink.consumer;
 
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import com.google.gson.Gson;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import ro.tucn.DataMode;
 import ro.tucn.consumer.AbstractGeneratorConsumer;
+import ro.tucn.flink.operator.batch.FlinkBatchOperator;
+import ro.tucn.flink.operator.batch.FlinkBatchPairOperator;
+import ro.tucn.generator.creator.GeneratorCreator;
+import ro.tucn.generator.generator.AbstractGenerator;
 import ro.tucn.kMeans.Point;
 import ro.tucn.operator.BatchOperator;
 import ro.tucn.operator.BatchPairOperator;
+import ro.tucn.util.Message;
 import ro.tucn.util.TimeHolder;
+import scala.Tuple2;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -14,30 +27,122 @@ import java.util.Properties;
  */
 public class FlinkGeneratorConsumer extends AbstractGeneratorConsumer {
 
-    private StreamExecutionEnvironment env;
+    protected AbstractGenerator generator;
+    private ExecutionEnvironment env;
 
-    public FlinkGeneratorConsumer(StreamExecutionEnvironment env) {
+    public FlinkGeneratorConsumer(ExecutionEnvironment env) {
         super();
         this.env = env;
     }
 
     @Override
-    public BatchPairOperator<String, String> getPairOperator(Properties properties, String topicPropertyName) {
+    public BatchPairOperator<String, String> getPairOperator(Properties properties,
+                                                             String topicPropertyName) {
+        //setEnvParallelism(parallelism);
+        DataSet<String> dataSetWithJsonAsValue = getStringWithJsonAsValueDatasetFromGenerator(properties, topicPropertyName);
+        DataSet<Tuple2<String, String>> pairDataSet = getPairDataSetFromDataSetWithJsonAsValue(dataSetWithJsonAsValue);
+        return new FlinkBatchPairOperator<String, String>(pairDataSet, parallelism);
+    }
+
+    @Override
+    public BatchOperator<TimeHolder<String>> getStringOperatorWithTimeHolder(Properties properties,
+                                                                             String topicPropertyName) {
         return null;
     }
 
     @Override
-    public BatchOperator<TimeHolder<String>> getStringOperatorWithTimeHolder(Properties properties, String topicPropertyName) {
-        return null;
+    public BatchOperator<Point> getPointOperator(Properties properties,
+                                                 String topicPropertyName) {
+        DataSet<String> jsonDataSet = getStringDataSetFromGenerator(properties, topicPropertyName);
+        DataSet<Point> pointDataSet = getPointDataSetFromJsonDataSet(jsonDataSet);
+        return new FlinkBatchOperator<Point>(pointDataSet, parallelism);
     }
 
     @Override
-    public BatchOperator<Point> getPointOperator(Properties properties, String topicPropertyName) {
-        return null;
+    public void askGeneratorToProduceData(String topic) {
+        generator = GeneratorCreator.getNewGenerator(topic, DataMode.BATCH, 0);
+        generator.generate(0);
     }
 
     @Override
-    public BatchOperator<String> getStringOperator(Properties properties, String topicPropertyName) {
-        return null;
+    public BatchOperator<String> getStringOperator(Properties properties,
+                                                   String topicPropertyName) {
+        DataSet<String> dataSetWithJsonAsValue = getStringWithJsonAsValueDataSetFromGenerator(properties, topicPropertyName);
+        DataSet<String> dataSet = getStringDataSetFromDataSetWithJsonAsValue(dataSetWithJsonAsValue);
+        return new FlinkBatchOperator<String>(dataSet, parallelism);
+    }
+
+    private DataSet<Tuple2<String,String>> getPairDataSetFromDataSetWithJsonAsValue(DataSet<String> dataSetWithJsonAsValue) {
+        DataSet<Tuple2<String, String>> pairDataSet = dataSetWithJsonAsValue.map(new MapFunction<String, Tuple2<String, String>>() {
+            @Override
+            public Tuple2<String, String> map(String s) throws Exception {
+                Gson gson = new Gson();
+                Message msg = gson.fromJson(s, Message.class);
+                return new Tuple2<String, String>(msg.getKey(), msg.getValue());
+            }
+        });
+        return pairDataSet;
+    }
+
+    private DataSet<String> getStringWithJsonAsValueDatasetFromGenerator(Properties properties, String topicPropertyName) {
+        DataSet<String> dataSet = getDataSetFromGenerator(properties, topicPropertyName);
+        return dataSet;
+    }
+
+    private DataSet<String> getStringDataSetFromGenerator(Properties properties, String topicPropertyName) {
+        DataSet<String> dataSetWithJsonAsValue = getStringWithJsonAsValueDataSetFromGenerator(properties, topicPropertyName);
+        DataSet<String> dataSet = getStringDataSetFromDataSetWithJsonAsValue(dataSetWithJsonAsValue);
+        return dataSet;
+    }
+
+    private DataSet<Point> getPointDataSetFromJsonDataSet(DataSet<String> jsonDataSet) {
+        DataSet<Point> pointDataSet = jsonDataSet.map(new MapFunction<String, Point>() {
+            @Override
+            public Point map(String s) throws Exception {
+                Gson gson = new Gson();
+                Point point = gson.fromJson(s, Point.class);
+                return point;
+            }
+        });
+        return pointDataSet;
+    }
+
+    private DataSet<String> getStringDataSetFromDataSetWithJsonAsValue(DataSet<String> dataSetWithJsonAsValue) {
+        DataSet<String> dataSet = dataSetWithJsonAsValue.map(new MapFunction<String, String>() {
+            @Override
+            public String map(String s) throws Exception {
+                Gson gson = new Gson();
+                Message msg = gson.fromJson(s, Message.class);
+                return msg.getValue();
+            }
+        });
+        return dataSet;
+    }
+
+    private DataSet<String> getStringWithJsonAsValueDataSetFromGenerator(Properties properties, String topicPropertyName) {
+        DataSet<String> dataSet = getDataSetFromGenerator(properties, topicPropertyName);
+        return dataSet;
+    }
+
+    private DataSet<String> getDataSetFromGenerator(Properties properties, String topicPropertyName) {
+        String topic = getTopicFromProperties(properties, topicPropertyName);
+        //AbstractGenerator generator = GeneratorCreator.getNewGenerator(topic, DataMode.BATCH, 0);
+        //generator.generate(0);
+        List<Map<String, String>> generatedData = generator.getGeneratedData(topic);
+        List<String> jsons = new ArrayList<>();
+        for (Map<String, String> map : generatedData) {
+            String json = map.get(null);
+            jsons.add(json);
+        }
+        DataSet<String> jsonDataSet = env.fromCollection(jsons);
+        return jsonDataSet;
+    }
+
+    private String getTopicFromProperties(Properties properties, String topicPropertyName) {
+        return properties.getProperty(topicPropertyName);
+    }
+
+    public void setGenerator(AbstractGenerator generator) {
+        this.generator = generator;
     }
 }
