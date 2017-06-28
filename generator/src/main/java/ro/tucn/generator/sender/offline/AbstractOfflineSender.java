a8a3a890cf89f281407e2192416d6c8880b2e9fb
Liviu <li.cira@yaho.com>
2017-06-28 12:14:58 +0300

    Added implementation.

--- generator/src/main/java/ro/tucn/generator/sender/offline/AbstractOfflineSender.java
+++ generator/src/main/java/ro/tucn/generator/sender/offline/AbstractOfflineSender.java
@@ -1,5 +1,9 @@
 package ro.tucn.generator.sender.offline;
 
+import org.apache.log4j.Logger;
+import ro.tucn.generator.GeneratorProducer;
+import ro.tucn.generator.GeneratorRecord;
+import ro.tucn.generator.helper.TimeHelper;
 import ro.tucn.generator.sender.AbstractSender;
 
 import java.io.Serializable;
@@ -8,5 +12,29 @@ import java.io.Serializable;
  * Created by Liviu on 6/28/2017.
  */
 public abstract class AbstractOfflineSender extends AbstractSender implements Serializable {
+
+    protected final Logger logger = Logger.getLogger(this.getClass().getSimpleName());
+
+    private GeneratorProducer<String, String> producer;
+    private GeneratorRecord<String, String> newRecord;
+
+    public AbstractOfflineSender() {
+        producer = new GeneratorProducer<>();
+    }
+
+    @Override
+    public void close() {
+        producer.close();
+    }
+
+    @Override
+    public void send(String topic, Object key, Object value) {
+        long timestamp = TimeHelper.getNanoTime();
+        newRecord = new GeneratorRecord(topic, timestamp, key, value);
+        producer.send(newRecord);
+        logger.info("Topic: " + topic +
+                "\tMessage: " + newRecord.getValue()
+        );
+    }
 
 }
