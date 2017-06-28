ac4e33c578d442b45006ff02c72ed52efbc0fc4b
Liviu <li.cira@yaho.com>
2017-06-28 14:29:04 +0300

    Added data mode.

--- flink/src/main/java/ro.tucn.flink/Application.java
+++ flink/src/main/java/ro.tucn.flink/Application.java
@@ -21,7 +21,7 @@ public class Application {
             ArgsParser.checkParamsValidityForTestBed(parsedArgs);
             String topic = ArgsParser.getTopic(parsedArgs);
             String mode = ArgsParser.getMode(parsedArgs);
-            ContextCreator contextCreator = new FlinkContextCreator(topic);
+            ContextCreator contextCreator = new FlinkContextCreator(topic, mode);
             WorkloadCreator workloadCreator = new WorkloadCreator();
             Workload workload = workloadCreator.getNewWorkload(contextCreator, topic, mode);
             workload.Start();
